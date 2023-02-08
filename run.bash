715ab62bd047cddc1da8258a869b07c51d839331
Sahan Reddy <sahan.reddy.58@gmail.com>
2023-02-08 14:19:49 -0500

    Updated run.bash

--- run.bash
+++ run.bash
@@ -4,12 +4,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SRC_DIR="${SCRIPT_DIR}/src"
 BUILD_DIR="${SCRIPT_DIR}/build"
 
+cd $SCRIPT_DIR
+
 if [[ ! -d $BUILD_DIR ]]; then
     mkdir "$BUILD_DIR"
 fi
 
-cd "$BUILD_DIR"
-javac -d . $SRC_DIR/*.java
+javac -d "$BUILD_DIR" $SRC_DIR/*.java
 
 PROGRAM="$1"
 if [[ -z $PROGRAM ]]; then
@@ -17,7 +18,7 @@ if [[ -z $PROGRAM ]]; then
     exit 1
 fi
 
-java $PROGRAM
+java -cp "$BUILD_DIR" "$PROGRAM"
 
 
 
