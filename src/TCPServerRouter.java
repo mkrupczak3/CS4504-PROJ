1321fdc02dd928684fb01d0afcabb59b9dcbf496
unknown <niwanoleiko@gmail.com>
2023-02-09 18:58:21 -0500

    Added TCPServerRouter comments
    I added extra documentation to TCPServerRouter.Java

--- src/TCPServerRouter.java
+++ src/TCPServerRouter.java
@@ -3,13 +3,29 @@ import java.net.*;
 
 public class TCPServerRouter {
   public static void main(String[] args) throws IOException {
+
+    // The TCP Server Router is in charge of accepting connections from the TCP Client/Server
+    // and creating threads for each connection. It also creates a routing
+    // table for the TCP Client/Server to use.
+
+    // A socket is an endpoint for communication between two machines.
+    // Here, Java has already provided an Object with optional methods for adjusting the behavior.
     Socket clientSocket = null; // socket for the thread
+    // Routing table 
+    // The routing table is a 2D array with 10 rows and 2 columns.
     Object[][] RoutingTable = new Object[10][2]; // routing table
+    // Note that a port and a socket are different things. A port is a number that identifies a
+    // specific process to which an incoming network message is to be delivered while a socket is
+    // an endpoint for communication.
     int SockNum = 5555; // port number
     Boolean Running = true;
     int ind = 0; // indext in the routing table
 
     // Accepting connections
+    // The ServerSocket class is used to create a server socket. A server socket waits for requests
+    // to come in over the network. It performs some operation based on that request, and then
+    // possibly returns a result to the requester.
+
     ServerSocket serverSocket = null; // server socket for accepting connections
     try {
       serverSocket = new ServerSocket(5555);
@@ -20,6 +36,9 @@ public class TCPServerRouter {
     }
 
     // Creating threads with accepted connections
+    // while the server is running, it will accept connections and create threads for each
+    // It prints out the address of the client/server that connected to the server router.
+    // If it can't connect, it runs into catch and exits
     while (Running == true) {
       try {
         clientSocket = serverSocket.accept();
