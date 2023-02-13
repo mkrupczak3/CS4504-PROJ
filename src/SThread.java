0d2a9866069fc1c7ed1ae3bce2ac06d1aad94428
Sahan Reddy <sahan.reddy.58@gmail.com>
2023-02-13 13:35:00 -0500

    Added rtable cleanup to thread exit code

--- src/SThread.java
+++ src/SThread.java
@@ -55,6 +55,8 @@ public class SThread extends Thread {
         }
 
         if (inputLine.equals("Bye.")) { // exit statement
+          RTable[index][0] = null;
+          RTable[index][1] = null;
           break; //Thomas. termination of connection
         }
       } // end while
