019847fb8bef5b8384f51a50ae8b9dac16e49fab
Sahan Reddy <sahan.reddy.58@gmail.com>
2023-02-13 21:16:08 -0500

    Change dist file structure

--- dist/repopulate-dist-src.bash
+++ dist/repopulate-dist-src.bash
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SRC_DIR="${SCRIPT_DIR}/src"
-DIST_DIR="${SCRIPT_DIR}/dist"
+SRC_DIR="${SCRIPT_DIR}/../src"
+DIST_DIR="${SCRIPT_DIR}/project"
 
 if [[ ! -d $DIST_DIR ]]; then
     echo "$DIST_DIR doesn't exist; creating..."
     mkdir "$DIST_DIR"
 fi
 
+echo "Source: $SRC_DIR"
+echo "Dest: $DIST_DIR"
 cp -r "$SRC_DIR" "$DIST_DIR"
-rm $DIST_DIR/src/*.class
